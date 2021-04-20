fdb65006e31177bb9c85cd382a4e25448abbdca7
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-20 15:48:22 -0400

    Create potential_revenue method for food_truck

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -22,4 +22,11 @@ class FoodTruck
       @inventory[item] = quantity
     end
   end
+
+  def potential_revenue
+    potential_revenue_per_item = @inventory.map do |item, quantity|
+      item.price_to_usd * quantity.to_f
+    end
+    potential_revenue_per_item.sum
+  end
 end
--- lib/item.rb
+++ lib/item.rb
@@ -8,5 +8,9 @@ class Item
     @price = attributes[:price]
   end
 
+  def price_to_usd
+    @price.slice!(0)
+    price_to_usd = @price.to_f
+  end
 
 end
