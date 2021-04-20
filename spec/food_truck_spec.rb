0f08dcf9837354ffbe3ec8ebd2e9247f6b28fed0
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-20 15:21:46 -0400

    Create test for check_stock method in food_truck class

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -20,7 +20,16 @@ RSpec.describe FoodTruck do
     it 'starts with an empty inventory' do
       expect(food_truck.inventory).to eq({})
     end
+  end
 
+  context 'methods' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    it 'can check stock of an item' do
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
   end
 
 end
